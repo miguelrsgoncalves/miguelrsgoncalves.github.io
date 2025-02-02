e7052b7fec8179874c2d71c2b746e47b5898d2b8
MiguelRSGoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-02-02 18:05:28 +0100

    Updates to the overall site funcitonality

---
+++ script.js
@@ -0,0 +1,63 @@
+var lastTab = 'home';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll("nav ul li a");
+    const mainContent = document.getElementById("main-content");
+  
+    function loadContent(tab) {
+        const tabName = tab.dataset.tab;
+
+        if(tabName == lastTab) return;
+        else lastTab = tabName;
+
+        const path = `tabs/${tabName}.html`;
+    
+        fetch(path)
+            .then(response => response.text())
+            .then(data => {
+                mainContent.innerHTML = data;
+                tabs.forEach(t => t.classList.remove("active"));
+                tab.classList.add("active");
+        
+                attachProjectLinks();
+            })
+            .catch(err => {
+            console.error("Error loading the content: ", err);
+            mainContent.innerHTML = "<p>Error loading content</p>";
+        });
+    }
+  
+    function attachProjectLinks() {
+      const projectLinks = document.querySelectorAll(".project-link");
+      projectLinks.forEach((link) => {
+        link.addEventListener("click", function (event) {
+          event.preventDefault();  // Prevent default navigation
+  
+          const projectURL = this.getAttribute("data-project");
+          console.log("Loading project from:", projectURL);
+  
+          loadProject(projectURL);
+        });
+      });
+    }
+  
+    const activeTab = document.querySelector("nav ul li a.active");
+    if (activeTab) {
+      loadContent(activeTab);
+    }
+  
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", function (event) {
+        event.preventDefault();  // Prevent default page navigation
+  
+        // Remove 'active' class from all tabs
+        tabs.forEach(t => t.classList.remove("active"));
+        // Add 'active' class to the clicked tab
+        tab.classList.add("active");
+  
+        // Load the corresponding content
+        loadContent(tab);
+      });
+    });
+  });
+  
