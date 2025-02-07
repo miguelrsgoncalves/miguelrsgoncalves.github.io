dbac2f82cfdcf545a2454cbabc48047da3226598
MiguelRSGoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-02-07 18:47:02 +0100

    p5 project cards adjusted

--- script.js
+++ script.js
@@ -68,46 +68,46 @@ function perTabLoad(tabName) {
   }
 }
 
-function loadWebGL(startButton, container, webGLGameHTML, loadingScreen) {
-  const webglContent = document.getElementById(container);
+function loadEmbeddedProject(startButton, container, embeddedProjectHTML, loadingScreen) {
+  const embeddedProjectContent = document.getElementById(container);
   const loadingScreenElement = document.getElementById(loadingScreen);
 
   if(startButton.innerHTML == 'Start') {
     loadingScreenElement.style.visibility = 'visible';
-    fetch(webGLGameHTML)
+    fetch(embeddedProjectHTML)
     .then(response => response.text())
     .then(data => {
       startButton.innerHTML = "Stop"
-      webglContent.innerHTML = data;
+      embeddedProjectContent.innerHTML = data;
     })
     .catch(error => {
       loadingScreenElement.style.visibility = 'hidden';
-      console.error("Error loading WebGL Game:", error);
-      webglContent.innerHTML = `<p>Error loading project. Please try again.</p>`;
+      console.error("Error loading embedded project:", error);
+      embeddedProjectContent.innerHTML = `<p>Error loading project. Please try again.</p>`;
     });
   } else {
     loadingScreenElement.style.visibility = 'hidden';
     startButton.innerHTML = "Start"
-    webglContent.innerHTML = "";
+    embeddedProjectContent.innerHTML = "";
   }
 }
 
-function restartWebGL(startButton, container, webGLGameHTML, loadingScreen) {
-  const webglContent = document.getElementById(container);
+function restartEmbeddedProject(startButton, container, embeddedProjectHTML, loadingScreen) {
+  const embeddedProjectContent = document.getElementById(container);
   const startButtonElement = document.getElementById(startButton);
   const loadingScreenElement = document.getElementById(loadingScreen);
 
   if(startButtonElement.innerHTML == 'Stop') {
     loadingScreenElement.style.visibility = 'visible';
-    fetch(webGLGameHTML)
+    fetch(embeddedProjectHTML)
     .then(response => response.text())
     .then(data => {
-      webglContent.innerHTML = data;
+      embeddedProjectContent.innerHTML = data;
     })
     .catch(error => {
       loadingScreenElement.style.visibility = 'hidden';
-      console.error("Error loading WebGL Game:", error);
-      webglContent.innerHTML = `<p>Error loading project. Please try again.</p>`;
+      console.error("Error loading embedded project:", error);
+      embeddedProjectContent.innerHTML = `<p>Error loading project. Please try again.</p>`;
     });
   }
 }
