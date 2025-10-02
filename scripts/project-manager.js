7d13661130821cea632f3fda58aee4e7fc7db503
Miguel RS Gonçalves <miguelrsgoncalves@miguelrsgoncalves.com>
2025-10-02 15:30:14 +0100

    Completed project card remodeling

--- scripts/project-manager.js
+++ scripts/project-manager.js
@@ -73,7 +73,7 @@ function replacePlaceholders(template, data) {
     return template.replace(/{{(.*?)}}/g, (match, p1) => {
         const key = p1.trim()
         if ((key === 'software' || key === 'roleCategory' || key === 'misc') && Array.isArray(data[key])) {
-            return data[key].join('<br>')
+            return data[key].map(item => `<span>${item}</span>`).join('')
         }
         return data[key] || ''
     })
