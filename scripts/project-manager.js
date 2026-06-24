9ada9d3393b26a6c3cd46fffc9b48e66fd8696eb
miguelrsgoncalves <miguelrsgoncalves@miguelrsgoncalves.com>
2026-06-24 18:54:09 +0100

    Fix projects show the thumbnail with new pathing

--- scripts/project-manager.js
+++ scripts/project-manager.js
@@ -70,8 +70,8 @@ async function populateProjects(containerId) {
 async function insertProjects() {
   await getProjectsData()
   document.querySelectorAll('[name="single-project-card"]').forEach(el => {
-    const pageName = el.getAttribute("page-name")
-    const projectCard = projectsData.find(element => element?.data?.["page-name"] === pageName)
+    const pageName = el.getAttribute("id")
+    const projectCard = projectsData.find(element => element?.data?.["id"] === pageName)
     el.replaceWith(document.createRange().createContextualFragment(projectCard.html))
   })
 }
